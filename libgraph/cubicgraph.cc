667e82e44c47aa7ef5607d7ed9608ae4dd513bb1
James Emil Avery <avery@diku.dk>
2017-12-16 20:33:17 +0100

    libgraph, adapted from Fullerene (http://tinyurl.com/fullerenes). wq

---
+++ libgraph/cubicgraph.cc
@@ -0,0 +1,134 @@
+#include "cubicgraph.hh"
+#include "triangulation.hh"
+
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+
+// the jumps start at 0
+// n is the number of vertices
+CubicGraph::CubicGraph(const int n, const vector<int>& spiral_string, const jumplist_t& jumps) : CubicGraph() {
+  assert(spiral_string.size() == n/2 + 2);
+  Triangulation dual(spiral_string,jumps);
+  CubicGraph G(dual.dual_graph());
+  *this = G;
+}
+
+CubicGraph::CubicGraph(const spiral_nomenclature &fsn){
+  assert(fsn.construction_scheme == spiral_nomenclature::CUBIC);
+  Triangulation T(fsn);
+  *this = T.dual_graph();
+}
+
+
+bool CubicGraph::get_spiral_from_cg(const node_t f1, const node_t f2, const node_t f3, vector<int> &spiral, jumplist_t &jumps, const bool general) const
+{
+  spiral.clear();
+  jumps.clear();
+  vector<node_t> permutation_dummy;
+
+  Triangulation Dual(this->dual_graph(6));
+
+  if(!Dual.get_spiral(f1, f2, f3, spiral, jumps, permutation_dummy, general)) return false;
+  assert(spiral.size()==N/2+2);
+  return true;
+}
+
+bool CubicGraph::get_spiral_from_cg(vector<int> &spiral, jumplist_t &jumps, const bool canonical, const bool general, const bool pentagon_start) const
+{
+  spiral.clear();
+  jumps.clear();
+
+  Triangulation Dual(this->dual_graph(6));
+
+  if(!Dual.get_spiral(spiral, jumps, general, pentagon_start)) return false;
+  assert(spiral.size()==N/2+2);
+  return true;
+}
+
+
+// returns the intersection of three sets, assuming that it's exactly one element (aimed at one vertex shared between three faces)
+// yes, I know this is inefficient, but the vectors are short
+int intersection_3(vector<int> &f1, vector<int> &f2, vector<int> &f3){
+  for (int i=0; i<f1.size(); i++){
+    for (int j=0; j<f2.size(); j++){
+      if (f1[i] != f2[j]) continue;
+      for (int k=0; k<f3.size(); k++){
+        if (f1[i] == f3[k]) return f1[i];
+      }
+    }
+  }
+  assert(false);
+  return -1;
+}
+
+
+// permutation of cub vertex numbers (ie, replace v by vertex_numbers[v], to get numbered vertices)
+// T is the dual of the cubic graph
+// perm is the permutation as returned by T.get_spiral()
+// locants are vertices that should have small vertex numbers (as far as permitted by symmetry equivalent canonical spirals)
+vector<int> CubicGraph::vertex_numbers(const Triangulation &T, const vector<vector<node_t>> &permutations, const vector<node_t> &locants) const{
+  vector<tri_t> tri_faces = T.compute_faces();
+  vector<node_t> vertex_numbers(N);
+  vector<node_t> vertex_numbers_inv(N,INT_MAX);
+
+  for(int p=0; p<permutations.size(); p++){
+    const vector<node_t> &perm=permutations[p];
+    // cout << "permutation of vertex numbers of triangulation: " << perm << endl;
+    vector<int> perm_inv(perm.size());
+    for(int i=0; i< perm.size(); i++) perm_inv[perm[i]] = i;
+    // cout << "inverse permutation of vertex numbers of triangulation: " << perm_inv << endl;
+
+    // cout << "triangles, relative to trig internal vertex numbers: " << tri_faces << endl;
+    for(int i=0; i<tri_faces.size(); i++){
+      for(int j=0; j<3; j++){
+        tri_faces[i][j] = perm_inv[tri_faces[i][j]];
+      }
+    }
+    // sort triples, first the vertices per face, then the faces
+    for(tri_t& t: tri_faces){
+      if(t[0] > t[1]) swap(t[0], t[1]);
+      if(t[1] > t[2]) swap(t[1], t[2]);
+      if(t[0] > t[1]) swap(t[0], t[1]);
+    }
+    sort(tri_faces.begin(), tri_faces.end());
+    // cout <<  "triangles, relative to canon vertex numbers: " << tri_faces << endl;
+
+    // permute back
+    for(int i=0; i<tri_faces.size(); i++){
+      for(int j=0; j<3; j++){
+        tri_faces[i][j] = perm[tri_faces[i][j]];
+      }
+    }
+    // cout << "triangles, in terms of internal numbers, sorted according to canon numbers: " << tri_faces << endl;
+
+    // and then find the vertex in cub, which is part of faces {0,1,2},{0,1,5}, ...
+    vector<face_t> cub_faces = compute_faces_oriented();
+    // cout << "faces " << cub_faces << endl;
+    vector<int> vertex_numbers_inv_tmp(N);
+    for(int v=0; v<N; v++){
+      face_t f0 = cub_faces[tri_faces[v][0]];
+      face_t f1 = cub_faces[tri_faces[v][1]];
+      face_t f2 = cub_faces[tri_faces[v][2]];
+      vertex_numbers_inv_tmp[v] = intersection_3(f0, f1, f2);
+    }
+    // copy to vertex_numbers_inv?
+    if(locants.size()==0){
+      vertex_numbers_inv = vertex_numbers_inv_tmp;
+      break;
+    }
+    for(int l=0; l<locants.size(); l++){
+      if(vertex_numbers_inv_tmp[locants[l]] > vertex_numbers_inv[locants[l]]) break;
+      if(vertex_numbers_inv_tmp[locants[l]] < vertex_numbers_inv[locants[l]]){
+        vertex_numbers_inv = vertex_numbers_inv_tmp;
+        break;
+      }
+    }
+  }
+  for(int i=0; i<vertex_numbers.size(); i++) vertex_numbers[vertex_numbers_inv[i]] = i;
+  // cout << "permutation of cub vertex numbers (ie, replace v by vertex_numbers[v], to get numbered vertices): " << vertex_numbers << endl;
+  return vertex_numbers;
+}
+
---
+++ libgraph/planargraph.hh
@@ -0,0 +1,128 @@
+#pragma once
+
+#include "graph.hh"
+#include "spiral.hh"
+
+// TODO: Separate planar cubic graph stuff away from general planar graph into CubicGraph class.
+//       Exploit duality between triangulation and cubic planar graph.
+class PlanarGraph : public Graph {
+public:
+  mutable face_t outer_face;
+  vector<coord2d> layout2d; 	// If graph is planar, we can associate a 2D layout
+  typedef spiral_nomenclature::construction_scheme_t construction_scheme_t;
+
+  // TODO: Get rid of layout_is_spherical
+  PlanarGraph() {}
+  PlanarGraph(const PlanarGraph& g) : Graph(g), layout2d(g.layout2d) {  }
+  PlanarGraph(const Graph& g, const node_t s=-1, const node_t t=0, const node_t r=0) : Graph(g)
+  {
+    if(s!=-1){ // Compute planar layout
+      layout2d = tutte_layout(s,t,r);
+      outer_face = find_outer_face();
+    } 
+  }
+  PlanarGraph(const spiral_nomenclature &fsn);
+
+ 
+  PlanarGraph(const Graph& g, const vector<coord2d>& layout) : Graph(g), layout2d(layout) {  }
+
+  
+  bool is_a_fullerene(bool verbose=false) const; // TODO: Do something better with output
+  bool is_cubic() const;
+  bool is_triangulation() const;
+
+  bool layout_is_crossingfree() const;
+
+  bool is_cut_vertex(const node_t v) const;
+
+  // This group of functions should be used whenever the graphs are oriented. Fmax is not necessary, just an extra back-stop.
+  vector<face_t> compute_faces_oriented(int Fmax=INT_MAX) const; // TODO: This should replace the old layout-based method  
+  face_t get_face_oriented(const dedge_t &e, int Fmax=INT_MAX) const; 
+  dedge_t get_face_representation(dedge_t e, int Fmax=INT_MAX) const; 
+  vector<dedge_t> compute_face_representations(int Fmax=INT_MAX) const; // Unique representation of face in oriented planar graph
+
+  // This should all be phased out. For non-oriented graphs, better to compute planar embedding and orient once and for all,
+  // than to use planar layout for orientation everywhere (or better yet, make sure graph is oriented in the first place).
+  vector<face_t> compute_faces(unsigned int Nmax=INT_MAX, bool planar_layout=false) const;
+  vector<face_t> compute_faces_layout_oriented() const;
+  face_t get_face_layout_oriented(int u, int v) const; // TODO: This uses the layout, should work towards retirement
+
+
+  void orient_neighbours();   // Ensures that neighbours are ordered CCW
+
+  face_t find_outer_face() const;
+
+
+  PlanarGraph dual_graph(unsigned int Fmax=INT_MAX, bool planar_layout=true) const;
+  // the dual of the LF, ie a Triangulation is returned
+  PlanarGraph leapfrog_dual() const;
+  // Every polyhedral graph G can be represented by a triangulation.
+  //  1. If G is a triangulation, it is G
+  //  2. If G is cubic, it is its dual
+  //  3. If G is non-cubic and non-triangulation, it is G's leapfrog dual
+  PlanarGraph enveloping_triangulation(construction_scheme_t &scheme) const;
+  
+  size_t count_perfect_matchings() const;
+
+
+  vector<node_t> vertex_numbers(vector<vector<node_t>> &perms, const vector<node_t> &loc) const;
+  
+  vector<tri_t>  triangulation(int face_max=INT_MAX) const;
+  vector<tri_t>  triangulation(const vector<face_t>& faces) const;
+  vector<tri_t>  centroid_triangulation(const vector<face_t>& faces) const ;
+  vector<tri_t>&  orient_triangulation(vector<tri_t>& tris) const;
+
+
+  vector<coord2d> tutte_layout(node_t s=0, node_t t=-1, node_t r=-1, unsigned int face_max=INT_MAX) const;
+  vector<coord2d> tutte_layout(const face_t& outer_face) const;
+  vector<coord2d> tutte_layout_iterative(const face_t& outer_face, const vector<coord2d>& outer_coords) const;
+  vector<coord2d> tutte_layout_direct(const face_t& outer_face, const vector<coord2d>& outer_coords) const;
+  vector<coord2d> spherical_projection() const;
+  bool optimize_layout(const double zv_dist=0.2, const double k_dist=10.0, const double k_angle=10.0, const double k_area=10.0);
+
+  vector<double> edge_lengths() const;
+  coord2d width_height() const;
+  void scale(const coord2d& x);
+  void move (const coord2d& x);
+
+  
+
+  vector<coord3d> zero_order_geometry(double scalerad=4) const;
+
+  // PlanarGraph I/O.
+  static vector<string> formats,input_formats, output_formats;
+  enum {ASCII,PLANARCODE,XYZ,MOL2,MATHEMATICA,LATEX} formats_t;
+  static int format_id(string id);
+  
+  static PlanarGraph from_file(string path, int index=0);
+  static PlanarGraph from_file(FILE *file, string format, int index=0);
+  static PlanarGraph from_ascii(FILE *file);
+  static PlanarGraph from_planarcode(FILE *file, const size_t index=0);
+  static PlanarGraph from_xyz(FILE *file);
+  static PlanarGraph from_mol2(FILE *file);
+
+  static bool to_file(const PlanarGraph &G, string path);
+  static bool to_file(const PlanarGraph &G, FILE *file, string format);
+  static bool to_ascii(const PlanarGraph &G, FILE *file);
+  static bool to_mathematica(const PlanarGraph &G, FILE *file);  
+  static bool to_planarcode(const PlanarGraph &G, FILE *file);    
+
+  static PlanarGraph read_hog_planarcode(FILE *planarcode_file);
+  static vector<PlanarGraph> read_hog_planarcodes(FILE *planarcode_file);
+  static bool read_hog_metadata(FILE *file, size_t &graph_count, size_t &graph_size);
+
+  string to_latex(double w_cm = 10, double h_cm = 10, bool show_dual = false, bool number_vertices = false, bool include_latex_header = false,
+		  int edge_colour = 0x6a5acd, int path_colour = 0x6a5acd, int vertex_colour = 0x8b2500,
+		  double edge_width = 0.1,double path_width = 0.1, double vertex_diameter = 2.0,
+		  int Npath = 0, int *path = 0
+		  ) const;
+
+  string to_povray(double w_cm = 10, double h_cm = 10, 
+		   int line_colour = 0x6a5acd, int vertex_colour = 0x8b2500,
+		   double line_width = 0.1, double vertex_diameter = 2.0
+		   ) const;
+
+  friend ostream& operator<<(ostream& s, const PlanarGraph& g);
+};
+
+
